54627cb53006132a7f90c483f85a20b20221e91c
adamthompson24 <adam@xactive.com>
2021-05-07 13:45:52 -0400

    testing 2

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
-pygame.init()
-win = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("shootingstar")
+import os
+import random
+import time
+# pygame.init()
+# win = pygame.display.set_mode((500, 500))
+# pygame.display.set_caption("shootingstar")
 
 # Hello
