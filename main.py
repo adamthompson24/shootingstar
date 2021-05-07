a3281fd7c54f69fa4e57485fd663a205a2c773be
adamthompson24 <adam@xactive.com>
2021-05-07 13:23:35 -0400

    print

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import pygame
+# import pygame
 
-pygame.init()
-win = pygame.display.set_mode((500, 500))
-
-# Hello Adam
-pygame.display.set_caption("shootingstar")
-
-# Hello
-#Hello
+# pygame.init()
+# win = pygame.display.set_mode((500, 500))
+# pygame.display.set_caption("shootingstar")
+ print('hello')
