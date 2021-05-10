bfca270416a1fca110b8ad513d3a2d5442bc2ee4
TylerMack2021 <83665879+TylerMack2021@users.noreply.github.com>
2021-05-10 13:34:41 -0400

    ...

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import pygame
 import os
 import random
 import time
+import sys
 pygame.init()
 win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("shootingstar")
+playing = True
+
+while playing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.exit()
+            sys.exit()
+            playing = False
+    pygame.display.update()
